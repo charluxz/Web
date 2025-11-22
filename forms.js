ec3eef4c514f3a7d9cd9532935cdbaf243aaf035
IK <IK@example.com>
2025-11-22 17:05:36 -0300

    Cambios para el commit de Git

--- forms.js
+++ forms.js
@@ -1,10 +1,29 @@
-const form = document.getElementById('myForm');
-
-form.addEventListener('submit', function(event) {
-  event.preventDefault(); // Evita la recarga de la página
-
-  const email = document.getElementById('email').value;
- 
-  console.log(`Correo Electrónico: ${email}`);
+document.querySelector('form')
+.addEventListener('submit', procesar => {
+    procesar.preventDefault();
+    const data = Object.fromEntries(new FormData(procesar.target).entries());
 });
+    registros.push(registro);
+function procesar() {
+    const email = document.getElementById('email').value;
+    console.log(`Correo electrónico suscrito: ${email}`);
+    const emailJSON = JSON.stringify(email);
+    localStorage.setItem('email', emailJSON);
+    const correo = {};
+for (let i = 0; i < localStorage.length; i++) {
+  const key = localStorage.key(i);
+  correo[key] = localStorage.getItem(key);
+}
 
+    const jsonString = JSON.stringify(correo, null, 2);
+    const blob = new Blob([jsonString], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'datos_suscripcion.json';
+    
+    a.click(); 
+URL.revokeObjectURL(url);
+
+
+}
