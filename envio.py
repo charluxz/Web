3a58867a34e3e74179f8e48de8ec286384e826bc
charluxz <gabrielcarlosmolpe@gmail.com>
2026-06-15 19:01:02 +0000

    .

--- envio.py
+++ envio.py
@@ -7,7 +7,7 @@ def procesar_datos():
 def enviarCorreo():
   import resend
   resend.api_key = "re_6LVBnd1c_H7D9nQRCTfrqy2AgMNWXvDbq"
-  correo = "gabrielcarlosmolpe@gmail.com"
+  correo = email
   r = resend.Emails.send({
   "from": "onboarding@resend.dev",
   "to": correo,
