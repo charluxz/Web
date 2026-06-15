27f5751197db05cb479bd55a61a9c49995afec69
charluxz <gabrielcarlosmolpe@gmail.com>
2026-06-14 23:06:30 -0400

    .

---
+++ resend.py
@@ -0,0 +1,11 @@
+import resend
+from secret import secreto
+
+resend.api_key = secreto
+
+r = resend.Emails.send({
+  "from": "onboarding@resend.dev",
+  "to": "gabrielcarlosmolpe@gmail.com",
+  "subject": "Hello World",
+  "html": "<p>Congrats on sending your <strong>first email</strong>!</p>"
+})
